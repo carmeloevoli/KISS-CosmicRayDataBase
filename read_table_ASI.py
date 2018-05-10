d75466e60b1fd637216de0777a8e154b5da127fb
Carmelo Evoli <carmelo.evoli@gssi.infn.it>
2018-05-10 15:11:04 +0200

    add reading table libs

---
+++ read_table_ASI.py
@@ -0,0 +1,69 @@
+from xml.dom import minidom
+import numpy as np
+
+def get_value(item, name):
+    elem = item.getElementsByTagName(name)[0]
+    return float(elem.firstChild.data)
+
+def compute_mean_rigidity(R_min, R_max, slope):
+    """ Compute the mean rigidity in the bin 
+    assuming a spectrum R^-slope 
+    """
+    rig_mean = (slope - 1.) / (slope - 2.);
+    rig_mean *= (R_max**(-slope + 2) - R_min**(-slope + 2)) / (R_max**(-slope + 1) - R_min**(-slope + 1)) 
+    return (rig_mean)
+
+def compute_total_error(stat_err, syst_err):
+    """ Compute total error given a statistycal error 
+    and a vector of systematic errors
+    """
+    total_systematical_error_squared = 0.
+    for s in syst_err:
+        total_systematical_error_squared += s * s;
+    return np.sqrt(total_systematical_error_squared + stat_err * stat_err)
+
+def get_table(filename):
+    tree = minidom.parse(filename)
+    items = tree.getElementsByTagName('DATA')
+    size = len(items)
+
+    print "read " + filename + " with data size : ", size
+
+    rig = np.zeros(size)
+    f = np.zeros(size)
+    f_err_low = np.zeros(size)
+    f_err_high = np.zeros(size)
+
+    counter = 0
+    for item in items:
+        rigidity_min = get_value(item, "rigidity_min")
+        rigidity_max = get_value(item, "rigidity_max")
+        rig[counter] = compute_mean_rigidity(rigidity_min, rigidity_max, 2.7)
+        
+        flux = get_value(item, "flux")
+        f[counter] = flux
+        
+        statistical_error_low = get_value(item, "flux_statistical_error_low")
+        statistical_error_high = get_value(item, "flux_statistical_error_high")
+
+        systematical_error_low = get_value(item, "flux_systematical_error_low")
+        systematical_error_high = get_value(item, "flux_systematical_error_high")
+
+        acc_error_low = get_value(item, "flux_acc_error_low")
+        acc_error_high = get_value(item, "flux_acc_error_high")
+
+        scale_error_low = get_value(item, "flux_scale_error_low")
+        scale_error_high = get_value(item, "flux_scale_error_high")
+
+        unf_error_low = get_value(item, "flux_unf_error_low")
+        unf_error_high = get_value(item, "flux_unf_error_high")
+
+        systematical_errors = np.array([systematical_error_low, acc_error_low, scale_error_low, unf_error_low]) 
+        f_err_low[counter] = compute_total_error(systematical_error_low, systematical_errors)
+
+        systematical_errors = np.array([systematical_error_high, acc_error_high, scale_error_high, unf_error_high]) 
+        f_err_high[counter] = compute_total_error(systematical_error_high, systematical_errors)
+        
+        counter = counter + 1
+
+    return rig, f, f_err_low, f_err_high
---
+++ read_table_CRDB.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def compute_mean_rigidity(R_min, R_max, slope):
+    """ Compute the mean rigidity in the bin 
+    assuming a spectrum R^-slope 
+    """
+    rig_mean = (slope - 1.) / (slope - 2.);
+    rig_mean *= (R_max**(-slope + 2) - R_min**(-slope + 2)) / (R_max**(-slope + 1) - R_min**(-slope + 1)) 
+    return (rig_mean)
+
+def compute_total_error(stat_err, syst_err):
+    """ Returns total error given statistycal and systematic errors
+        Args:
+          stat_err (float): 
+          syst_err (float):
+        Returns:
+          (float): total error :math:`\\sqrt(stat^2 + syst^2)`
+        """
+ 
+    """
+    return np.sqrt(syst_err * syst_err + stat_err * stat_err)
+
+def get_table(filename):
+    R_low, R_up, y, y_stat_lo, y_stat_up, y_syst_lo, y_syst_up = np.loadtxt(filename,skiprows=2,usecols=(1,2,3,4,5,6,7),unpack=True)
+    
+    print "read " + filename + " with data size : ", len(y)
+    
+    rig = compute_mean_rigidity(R_low, R_up, 2.7)
+    f = y
+
+    f_err_low = compute_total_error(y_stat_lo, y_syst_lo)
+    f_err_high = compute_total_error(y_stat_up, y_syst_up)
+
+    return rig, f, f_err_low, f_err_high
