665d88e4404cf6772ffa9f4dba3b3921a9ece2f8
Carmelo Evoli <carmelo.evoli@gssi.it>
2020-06-03 11:40:32 +0200

    fixed error in double-counting the systematic error

--- generate_tables.py
+++ generate_tables.py
@@ -7,6 +7,10 @@ import numpy as np
 credit_asi = "# credit: SSDC Cosmic Ray Database tools.asdc.asi.it/CosmicRays \n"
 credit_crdb = "# credit: CRDB Database lpsc.in2p3.fr/cosmic-rays-db \n"
 
+def data_protons_AMS02(ssdc_filename, output_filename):
+    R, dNdR, dNdR_err_low, dNdR_err_high = ASI.get_table_H(ssdc_filename)
+    wt.write_rigidity_table(credit_asi, output_filename, R, dNdR, dNdR_err_low, dNdR_err_high)
+
 def data_nuclei_AMS02(ssdc_filename, output_filename):
     R, dNdR, dNdR_err_low, dNdR_err_high = ASI.get_table_flux(ssdc_filename)
     wt.write_rigidity_table(credit_asi, output_filename, R, dNdR, dNdR_err_low, dNdR_err_high)
@@ -16,7 +20,7 @@ def data_ratio_AMS02(ssdc_filename, output_filename):
     wt.write_rigidity_table(credit_asi, output_filename, R, ratio, ratio_err_low, ratio_err_high)
 
 ### DATABASE
-data_nuclei_AMS02("ASI/p_AMS_PRL2015_000.xml", "kiss_tables/H_AMS02_rigidity.txt")
+data_protons_AMS02("ASI/p_AMS_PRL2015_000.xml", "kiss_tables/H_AMS02_rigidity.txt")
 data_nuclei_AMS02("ASI/He_AMSPRL2017_rig_000.xml", "kiss_tables/He_AMS02_rigidity.txt")
 data_nuclei_AMS02("ASI/C_AMSPRL2017_rig_000.xml", "kiss_tables/C_AMS02_rigidity.txt")
 data_nuclei_AMS02("ASI/O_AMSPRL2017_rig_000.xml", "kiss_tables/O_AMS02_rigidity.txt")
--- read_table_ASI.py
+++ read_table_ASI.py
@@ -46,8 +46,8 @@ def get_table_flux(filename):
         statistical_error_low = get_value(item, "flux_statistical_error_low")
         statistical_error_high = get_value(item, "flux_statistical_error_high")
 
-        systematical_error_low = get_value(item, "flux_systematical_error_low")
-        systematical_error_high = get_value(item, "flux_systematical_error_high")
+#        systematical_error_low = get_value(item, "flux_systematical_error_low")
+#        systematical_error_high = get_value(item, "flux_systematical_error_high")
 
         acc_error_low = get_value(item, "flux_acc_error_low")
         acc_error_high = get_value(item, "flux_acc_error_high")
@@ -57,11 +57,60 @@ def get_table_flux(filename):
 
         unf_error_low = get_value(item, "flux_unf_error_low")
         unf_error_high = get_value(item, "flux_unf_error_high")
-
-        systematical_errors = np.array([systematical_error_low, acc_error_low, scale_error_low, unf_error_low]) 
+        
+        systematical_errors = np.array([acc_error_low, scale_error_low, unf_error_low])
         f_err_low[counter] = compute_total_error(statistical_error_low, systematical_errors)
 
-        systematical_errors = np.array([systematical_error_high, acc_error_high, scale_error_high, unf_error_high]) 
+        systematical_errors = np.array([acc_error_high, scale_error_high, unf_error_high])
+        f_err_high[counter] = compute_total_error(statistical_error_high, systematical_errors)
+        
+        counter = counter + 1
+
+    return rig, f, f_err_low, f_err_high
+
+def get_table_H(filename):
+    tree = minidom.parse(filename)
+    items = tree.getElementsByTagName('DATA')
+    size = len(items)
+
+    print ("read " + filename + " with data size : ", size)
+
+    rig = np.zeros(size)
+    f = np.zeros(size)
+    f_err_low = np.zeros(size)
+    f_err_high = np.zeros(size)
+
+    counter = 0
+    for item in items:
+        rigidity_min = get_value(item, "rigidity_min")
+        rigidity_max = get_value(item, "rigidity_max")
+        rig[counter] = compute_mean_rigidity(rigidity_min, rigidity_max, 2.7)
+        
+        flux = get_value(item, "flux")
+        f[counter] = flux
+        
+        statistical_error_low = get_value(item, "flux_statistical_error_low")
+        statistical_error_high = get_value(item, "flux_statistical_error_high")
+
+#        systematical_error_low = get_value(item, "flux_systematical_error_low")
+#        systematical_error_high = get_value(item, "flux_systematical_error_high")
+
+        acc_error_low = get_value(item, "flux_acc_error_low")
+        acc_error_high = get_value(item, "flux_acc_error_high")
+
+        scale_error_low = get_value(item, "flux_scale_error_low")
+        scale_error_high = get_value(item, "flux_scale_error_high")
+
+        unf_error_low = get_value(item, "flux_unf_error_low")
+        unf_error_high = get_value(item, "flux_unf_error_high")
+        
+        trig_error_low = get_value(item, "flux_trig_error_low")
+        trig_error_high = get_value(item, "flux_trig_error_high")
+
+        systematical_errors = np.array([acc_error_low, scale_error_low, unf_error_low, trig_error_low])
+        f_err_low[counter] = compute_total_error(statistical_error_low, systematical_errors)
+
+        systematical_errors = np.array([acc_error_high, scale_error_high, unf_error_high, trig_error_high])
         f_err_high[counter] = compute_total_error(statistical_error_high, systematical_errors)
         
         counter = counter + 1
@@ -92,8 +141,8 @@ def get_table_ratio(filename):
         statistical_error_low = get_value(item, "fluxratio_statistical_error_low")
         statistical_error_high = get_value(item, "fluxratio_statistical_error_high")
 
-        systematical_error_low = get_value(item, "fluxratio_systematical_error_low")
-        systematical_error_high = get_value(item, "fluxratio_systematical_error_high")
+#        systematical_error_low = get_value(item, "fluxratio_systematical_error_low")
+#        systematical_error_high = get_value(item, "fluxratio_systematical_error_high")
 
         acc_error_low = get_value(item, "fluxratio_acc_error_low")
         acc_error_high = get_value(item, "fluxratio_acc_error_high")
@@ -104,10 +153,10 @@ def get_table_ratio(filename):
         unf_error_low = get_value(item, "fluxratio_unf_error_low")
         unf_error_high = get_value(item, "fluxratio_unf_error_high")
 
-        systematical_errors = np.array([systematical_error_low, acc_error_low, scale_error_low, unf_error_low])
+        systematical_errors = np.array([acc_error_low, scale_error_low, unf_error_low])
         r_err_low[counter] = compute_total_error(statistical_error_low, systematical_errors)
 
-        systematical_errors = np.array([systematical_error_high, acc_error_high, scale_error_high, unf_error_high])
+        systematical_errors = np.array([acc_error_high, scale_error_high, unf_error_high])
         r_err_high[counter] = compute_total_error(statistical_error_high, systematical_errors)
     
         counter = counter + 1
