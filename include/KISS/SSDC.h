2589ccc1dd93f61313ca139b46eec195dc8b4aff
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-09-07 15:48:54 +0200

    improve readability

--- include/KISS/SSDC.h
+++ include/KISS/SSDC.h
@@ -8,6 +8,9 @@
 
 namespace KISS {
 
+bool IsFlux(YQuantities yQuantity);
+bool IsRatio(YQuantities yQuantity);
+
 class SSDC : public CrDataset {
    public:
     explicit SSDC(CrExperiments crExperiment, XQuantities xQuantity, YQuantities yQuantity, EnergyModes mode)
@@ -20,9 +23,6 @@ class SSDC : public CrDataset {
 
    protected:
     std::string makeSourceFilename() const override;
-    const std::vector<YQuantities> fluxes{H, He, Li, Be, B, C, N, O, F, Ne, Mg, Si, Fe};
-    const std::vector<YQuantities> ratios{B_C,  B_O,  Be_B, Be_C, Be_O, C_O, F_B, F_Si,  Fe_He, Fe_O,  Fe_Si, H_He,
-                                          He_O, Li_B, Li_C, Li_O, Mg_O, N_B, N_O, Ne_Mg, Ne_O,  Si_Mg, Si_O};
 };
 
 }  // namespace KISS
--- src/SSDC.cpp
+++ src/SSDC.cpp
@@ -5,7 +5,41 @@
 
 #include "KISS/utils.h"
 #include "pugixml/pugixml.hpp"
+
 namespace KISS {
+
+bool IsFlux(YQuantities yQuantity) {
+    const std::vector<YQuantities> fluxes{H, He, Li, Be, B, C, N, O, F, Ne, Mg, Si, Fe, electron, positron, lepton};
+    return (std::find(fluxes.begin(), fluxes.end(), yQuantity) != fluxes.end());
+}
+
+bool IsRatio(YQuantities yQuantity) {
+    const std::vector<YQuantities> ratios{positronfraction,
+                                          B_C,
+                                          B_O,
+                                          Be_B,
+                                          Be_C,
+                                          Be_O,
+                                          C_O,
+                                          F_B,
+                                          F_Si,
+                                          Fe_He,
+                                          Fe_O,
+                                          Fe_Si,
+                                          H_He,
+                                          He_O,
+                                          Li_B,
+                                          Li_C,
+                                          Li_O,
+                                          Mg_O,
+                                          N_B,
+                                          N_O,
+                                          Ne_Mg,
+                                          Ne_O,
+                                          Si_Mg,
+                                          Si_O};
+    return (std::find(ratios.begin(), ratios.end(), yQuantity) != ratios.end());
+}
 
 std::string SSDC::makeSourceFilename() const {
     std::string s = "source/";
@@ -22,6 +56,8 @@ void SSDC::readfile(std::string filename) {
     if (!result) throw std::runtime_error("error in loadig the XML file.");
     const auto tree = doc.child("XML");
 
+    size_t n = std::distance(tree.children("DATA").begin(), tree.children("DATA").end());
+
     for (pugi::xml_node node : tree.children("DATA")) {
         double xMean = 0;
 
@@ -29,11 +65,17 @@ void SSDC::readfile(std::string filename) {
             double x_min = node.child("rigidity_min").first_child().text().as_double();
             double x_max = node.child("rigidity_max").first_child().text().as_double();
             xMean = Utils::computeMeanEnergy(x_min, x_max, m_energyMode);
+        } else if (m_xQuantity == kEnergy) {
+            double x_min = node.child("kinetic_energy_min").first_child().text().as_double();
+            double x_max = node.child("kinetic_energy_min").first_child().text().as_double();
+            xMean = Utils::computeMeanEnergy(x_min, x_max, m_energyMode);
+        } else {
+            throw std::runtime_error("energy value not valid");
         }
 
-        double y, statLo, statUp, errTotLo, errTotUp;
+        double y = -1, statLo = 0, statUp = 0, errTotLo = 0, errTotUp = 0;
 
-        if (std::find(fluxes.begin(), fluxes.end(), m_yQuantity) != fluxes.end()) {
+        if (IsFlux(m_yQuantity)) {
             double systLo, systUp;
             y = node.child("flux").first_child().text().as_double();
             statLo = node.child("flux_statistical_error_low").first_child().text().as_double();
@@ -42,7 +84,7 @@ void SSDC::readfile(std::string filename) {
             systUp = node.child("flux_systematical_error_high").first_child().text().as_double();
             errTotLo = Utils::quadrature(statLo, systLo);
             errTotUp = Utils::quadrature(statUp, systUp);
-        } else if (std::find(ratios.begin(), ratios.end(), m_yQuantity) != ratios.end()) {
+        } else if (IsRatio(m_yQuantity)) {
             double systLo, systUp;
             y = node.child("fluxratio").first_child().text().as_double();
             statLo = node.child("fluxratio_statistical_error_low").first_child().text().as_double();
@@ -51,11 +93,16 @@ void SSDC::readfile(std::string filename) {
             systUp = node.child("fluxratio_systematical_error_high").first_child().text().as_double();
             errTotLo = Utils::quadrature(statLo, systLo);
             errTotUp = Utils::quadrature(statUp, systUp);
-        }
+        } else
+            throw std::invalid_argument("y-quantity not valid");
 
-        dataPoint data = {{xMean, y}, {statLo, statUp}, {errTotLo, errTotUp}};
-        m_dataTable.push_back(data);
+        if (y > 0.) {
+            dataPoint data = {{xMean, y}, {statLo, statUp}, {errTotLo, errTotUp}};
+            m_dataTable.push_back(data);
+        }
     }
+
+    if (n != m_dataTable.size()) throw std::runtime_error("generic problem in reading the XML file");
 }
 
 }  // namespace KISS
