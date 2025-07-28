cee115934a5300727d3134f0cfb2462db7fc77d3
carmeloevoli <carmelo.evoli@gssi.it>
2025-07-28 14:28:26 +0200

    crdb api

---
+++ source/get_crdb_data.py
@@ -0,0 +1,221 @@
+import crdb
+
+def print_column_names(tab):
+    for icol, col_name in enumerate(tab.dtype.fields):
+        print("%2i" % icol, col_name)
+
+def dump_datafile(quantity, energyType, expName, subExpName, filename, combo_level=0):
+    filename = 'CRDB/' + filename
+    print(f"search for {quantity} as a function of {energyType} measured by {expName}")
+    
+    tab = crdb.query(quantity, energy_type=energyType, combo_level=combo_level, energy_convert_level=0, exp_dates=expName)
+ 
+    subExpNames = set(tab["sub_exp"])
+    print("number of datasets found : ", len(subExpNames))
+    print(subExpNames)
+
+    adsCodes = set(tab["ads"])
+    print(adsCodes)
+
+    items = [i for i in range(len(tab["sub_exp"])) if tab["sub_exp"][i] == subExpName]
+    print("number of data : ", len(items))
+    assert(len(items) > 0)
+
+    print(f"dump on {filename}")
+    with open(filename, 'w') as f:
+#        f.write(f"#source: CRDB\n")
+#        f.write(f"#Quantity: {quantity}\n")
+#        f.write(f"#EnergyType: {energyType}\n")
+#        f.write(f"#Experiment: {expName}\n")
+        f.write(f"#ads: {tab['ads'][items[0]]}\n")
+        f.write(f"#E_lo - E_up - y - errSta_lo - errSta_up - errSys_lo - errSys_up\n")
+        for eBin, value, errSta, errSys in zip(tab["e_bin"][items], tab["value"][items], tab["err_sta"][items], tab["err_sys"][items]):
+            f.write(f"{eBin[0]:10.5e} {eBin[1]:10.5e} {value:10.5e} {errSta[0]:10.5e} {errSta[1]:10.5e} {errSys[0]:10.5e} {errSys[1]:10.5e}\n")
+    print("")
+    
+    
+def AMS02():
+    dump_datafile("e-", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_e-_rigidity.txt")
+    dump_datafile("e+", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_e+_rigidity.txt")
+    dump_datafile("e-+e+", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_e+e-_rigidity.txt")
+    dump_datafile("e+/e-+e+", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_posfraction_rigidity.txt")
+    dump_datafile("1H-bar", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_pbar_rigidity.txt")
+    dump_datafile("1H-bar/e+", "R", "AMS02", "AMS02 (2011/05-2015/05)", "AMS-02_pbar_e+_rigidity.txt")
+    dump_datafile("1H-bar/e+", "R", "AMS02", "AMS02 (2011/05-2015/05)", "AMS-02_pbar_e+_rigidity.txt")
+    dump_datafile("1H/e+", "R", "AMS02", "AMS02 (2011/05-2015/05)", "AMS-02_H_e+_rigidity.txt")
+    dump_datafile("1H/e-", "R", "AMS02", "AMS02 (2011/05-2015/05)", "AMS-02_H_e-_rigidity.txt")
+    dump_datafile("H", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_H_rigidity.txt")
+    dump_datafile("He", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_He_rigidity.txt")
+    dump_datafile("Li", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Li_rigidity.txt")
+    dump_datafile("Be", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Be_rigidity.txt")
+    dump_datafile("B", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_B_rigidity.txt")
+    dump_datafile("C", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_C_rigidity.txt")
+    dump_datafile("N", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_N_rigidity.txt")
+    dump_datafile("O", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_O_rigidity.txt")
+    dump_datafile("F", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_F_rigidity.txt")
+    dump_datafile("Ne", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_Ne_rigidity.txt")
+    dump_datafile("Na", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_Na_rigidity.txt")
+    dump_datafile("Mg", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_Mg_rigidity.txt")
+    dump_datafile("Al", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_Al_rigidity.txt")
+    dump_datafile("Si", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_Si_rigidity.txt")
+    dump_datafile("S", "R", "AMS02", "AMS02 (2011/05-2021/05)", "AMS-02_S_rigidity.txt")
+    dump_datafile("Fe", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_Fe_rigidity.txt")
+    dump_datafile("H/He", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_H_He_rigidity.txt")
+    dump_datafile("He/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_He_O_rigidity.txt")
+    dump_datafile("Li/B", "R", "AMS02", "AMS02 (2011/05-2016/05)", "AMS-02_Li_B_rigidity.txt")
+    dump_datafile("Li/C", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Li_C_rigidity.txt")
+    dump_datafile("Li/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Li_O_rigidity.txt")
+    dump_datafile("Be/B", "R", "AMS02", "AMS02 (2011/05-2016/05)", "AMS-02_Be_B_rigidity.txt")
+    dump_datafile("Be/C", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Be_C_rigidity.txt")
+    dump_datafile("Be/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Be_O_rigidity.txt")
+    dump_datafile("B/C", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_B_C_rigidity.txt")
+    dump_datafile("B/C", "EKN", "AMS02", "AMS02 (2011/05-2016/05)", "AMS-02_B_C_kineticEnergyPerNucleon.txt")
+    dump_datafile("B/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_B_O_rigidity.txt")
+    dump_datafile("C/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_C_O_rigidity.txt")
+    dump_datafile("N/B", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_N_B_rigidity.txt")
+    dump_datafile("N/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_N_O_rigidity.txt")
+    dump_datafile("F/B", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_F_B_rigidity.txt")
+    dump_datafile("F/Si", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_F_Si_rigidity.txt")
+    dump_datafile("Ne/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Ne_O_rigidity.txt")
+    dump_datafile("Ne/Mg", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Ne_Mg_rigidity.txt")
+    dump_datafile("Mg/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Mg_O_rigidity.txt")
+    dump_datafile("Si/O", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Si_O_rigidity.txt")
+    dump_datafile("Si/Mg", "R", "AMS02", "AMS02 (2011/05-2018/05)", "AMS-02_Si_Mg_rigidity.txt")
+    dump_datafile("Fe/He", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_Fe_He_rigidity.txt")
+    dump_datafile("Fe/O", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_Fe_O_rigidity.txt")
+    dump_datafile("Fe/Si", "R", "AMS02", "AMS02 (2011/05-2019/10)", "AMS-02_Fe_Si_rigidity.txt")
+
+def ATIC():
+    dump_datafile("H", "ETOT", "ATIC", "ATIC02 (2003/01)", "ATIC-02_H_totalEnergy.txt")
+
+def BESS():
+    dump_datafile("H", "EKN", "BESS-TeV", "BESS-TeV (2002/08)", "BESS-TeV_H_kineticEnergy.txt")
+    dump_datafile("He", "EKN", "BESS-TeV", "BESS-TeV (2002/08)", "BESS-TeV_He_kineticEnergyPerNucleon.txt")
+    dump_datafile("1H-bar", "EKN", "BESS-PolarII", "BESS-PolarII (2007/12-2008/01)", "BESS-PolarII_pbar_kineticEnergy.txt")
+
+def CALET():
+    dump_datafile("e-+e+", "ETOT", "CALET", "CALET (2015/10-2017/11)", "CALET_e+e-_totalEnergy.txt")
+    dump_datafile("H", "EKN", "CALET", "CALET (2015/10-2021/12)", "CALET_H_kineticEnergy.txt")
+    dump_datafile("He", "EK", "CALET", "CALET (2015/10-2022/04)", "CALET_He_kineticEnergy.txt")
+    dump_datafile("H/He", "R", "CALET", "CALET (2015/10-2022/04)", "CALET_H_He_rigidity.txt")
+    dump_datafile("H/He", "EKN", "CALET", "CALET (2015/10-2022/04)", "CALET_H_He_kineticEnergyPerNucleon.txt")
+    dump_datafile("B", "EKN", "CALET", "CALET (2015/10-2022/02)", "CALET_B_kineticEnergyPerNucleon.txt")
+    dump_datafile("C", "EKN", "CALET", "CALET (2015/10-2022/02)", "CALET_C_kineticEnergyPerNucleon.txt")
+    dump_datafile("O", "EKN", "CALET", "CALET (2015/10-2019/10)", "CALET_O_kineticEnergyPerNucleon.txt")
+    dump_datafile("Fe", "EKN", "CALET", "CALET (2016/01-2020/05)", "CALET_Fe_kineticEnergyPerNucleon.txt")
+    dump_datafile("Ni", "EKN", "CALET", "CALET (2015/11-2021/05)", "CALET_Ni_kineticEnergyPerNucleon.txt")
+    dump_datafile("B/C", "EKN", "CALET", "CALET (2015/10-2022/02)", "CALET_B_C_kineticEnergyPerNucleon.txt")
+    dump_datafile("C/O", "EKN", "CALET", "CALET (2015/10-2019/10)", "CALET_C_O_kineticEnergyPerNucleon.txt")
+
+def CREAM():
+    dump_datafile("H", "EKN", "CREAM", "CREAM-I+III (2004+2007)", "CREAM_H_kineticEnergy.txt")
+    dump_datafile("H", "EKN", "CREAM", "ISS-CREAM (2017/08-2019/02)", "ISS-CREAM_H_kineticEnergy.txt")
+    dump_datafile("He", "EKN", "CREAM", "CREAM-I+III (2004+2007)", "CREAM_He_kineticEnergyPerNucleon.txt")
+    dump_datafile("C", "EKN", "CREAM", "CREAM-II (2005/12-2006/01)", "CREAM_C_kineticEnergyPerNucleon.txt")
+    dump_datafile("O", "EKN", "CREAM", "CREAM-II (2005/12-2006/01)", "CREAM_O_kineticEnergyPerNucleon.txt")
+    dump_datafile("Ne", "EKN", "CREAM", "CREAM-II (2005/12-2006/01)", "CREAM_Ne_kineticEnergyPerNucleon.txt")
+    dump_datafile("Mg", "EKN", "CREAM", "CREAM-II (2005/12-2006/01)", "CREAM_Mg_kineticEnergyPerNucleon.txt")
+    dump_datafile("Si", "EKN", "CREAM", "CREAM-II (2005/12-2006/01)", "CREAM_Si_kineticEnergyPerNucleon.txt")
+    dump_datafile("Fe", "EKN", "CREAM", "CREAM-II (2005/12-2006/01)", "CREAM_Fe_kineticEnergyPerNucleon.txt")
+    dump_datafile("B/C", "EKN", "CREAM", "CREAM-I (2004/12-2005/01)", "CREAM_B_C_kineticEnergyPerNucleon.txt")
+
+def DAMPE():
+    dump_datafile("e-+e+", "ETOT", "DAMPE", "DAMPE (2015/12-2017/06)", "DAMPE_e+e-_totalEnergy.txt")
+    dump_datafile("H", "ETOT", "DAMPE", "DAMPE (2016/01-2018/06)", "DAMPE_H_totalEnergy.txt")
+    dump_datafile("He", "ETOT", "DAMPE", "DAMPE (2016/01-2020/06)", "DAMPE_He_totalEnergy.txt")
+    dump_datafile("B/C", "EKN", "DAMPE", "DAMPE (2016/01-2021/12)", "DAMPE_B_C_kineticEnergyPerNucleon.txt")
+    dump_datafile("B/O", "EKN", "DAMPE", "DAMPE (2016/01-2021/12)", "DAMPE_B_O_kineticEnergyPerNucleon.txt")
+
+def FERMI():
+    dump_datafile("e+", "EK", "FERMI", "Fermi-LAT  (2008/06-2011/04)", "FERMI_e+_kineticEnergy.txt")
+    dump_datafile("e-+e+", "ETOT", "FERMI", "Fermi-LAT-HE (2008/08-2015/06)", "FERMI_e+e-_totalEnergy.txt")
+    dump_datafile("e+/e-+e+", "EK", "FERMI", "Fermi-LAT  (2008/06-2011/04)", "FERMI_posfraction_totalEnergy.txt")
+
+def GAMMA():
+    dump_datafile("AllParticles", "ETOT", "GAMMA", "GAMMA (2003/01-2007/12) SIBYLL", "GAMMA_allParticle_totalEnergy.txt")
+    dump_datafile("H", "ETOT", "GAMMA", "GAMMA (2003/01-2007/12) SIBYLL", "GAMMA_H_totalEnergy.txt")
+    dump_datafile("He", "ETOT", "GAMMA", "GAMMA (2003/01-2007/12) SIBYLL", "GAMMA_He_totalEnergy.txt")
+
+def HAWC():
+    dump_datafile("AllParticles", "ETOT", "HAWC", "HAWC (2018-2019) QGSJet-II-04", "HAWC_allParticle_totalEnergy.txt")
+
+def HESS():
+    dump_datafile("e-+e+", "EK", "HESS", "H.E.S.S.-LE (2004/10-2005/12)", "HESS-LE_e+e-_kineticEnergy.txt")
+    dump_datafile("e-+e+", "EK", "HESS", "H.E.S.S. (2004/10-2007/08)", "HESS-HE_e+e-_kineticEnergy.txt")
+    dump_datafile("Fe", "ETOT", "HESS", "H.E.S.S. (2004/01-2006/12) QGSJet01+SIBYLL2.1", "HESS_Fe_totalEnergy.txt")
+
+def ICETOP():
+    dump_datafile("H", "ETOT", "IceTop", "IceCube+IceTop (2010/06-2013/05) SIBYLL2.1", "IceTop_IceCube_SIBYLL-2.1_H_totalEnergy.txt")
+    dump_datafile("He", "ETOT", "IceTop", "IceCube+IceTop (2010/06-2013/05) SIBYLL2.1", "IceTop_IceCube_SIBYLL-2.1_He_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "IceTop", "IceCube+IceTop (2010/06-2013/05) SIBYLL2.1", "IceTop_IceCube_SIBYLL-2.1_allParticle_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "IceTop", "IceTop (2010/06-2013/05)", "IceTop_allParticle_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "IceTop", "IceTop (2016/05-2017/04) SIBYLL2.1", "IceTop_SIBYLL-2.1_allParticle_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "IceTop", "IceTop (2016/05-2017/04) QGSJet-II-04", "IceTop_QGSJet-II-04_allParticle_totalEnergy.txt")
+
+def KASCADE():
+    dump_datafile("H", "ETOT", "KASCADE", "KASCADE (1996/10-2002/01) SIBYLL 2.1", "KASCADE_SIBYLL-2.1_H_totalEnergy.txt")
+    dump_datafile("H", "ETOT", "KASCADE", "KASCADE (1996/10-2002/01) QGSJet01", "KASCADE_QGSJet-01_H_totalEnergy.txt")
+    dump_datafile("He", "ETOT", "KASCADE", "KASCADE (1996/10-2002/01) SIBYLL 2.1", "KASCADE_SIBYLL-2.1_He_totalEnergy.txt")
+    dump_datafile("He", "ETOT", "KASCADE", "KASCADE (1996/10-2002/01) QGSJet01", "KASCADE_QGSJet-01_He_totalEnergy.txt")
+
+def KASCADE_Grande():
+    dump_datafile("H-He-group", "ETOT", "KASCADE-Grande", "KASCADE-Grande (2003/01-2009/03) QGSJet-II-04", "KASCADE-Grande_QGSJet-II-04_light_totalEnergy.txt")
+    dump_datafile("H-He-group", "ETOT", "KASCADE-Grande", "KASCADE-Grande (2004/03-2012/05) QGSJet-II-2", "KASCADE-Grande_QGSJet-II-02_light_totalEnergy.txt")
+    dump_datafile("H-He-group", "ETOT", "KASCADE-Grande", "KASCADE-Grande (2004/03-2010/12) QGSJet-II-3", "KASCADE-Grande_QGSJet-II-03_light_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "KASCADE-Grande", "KASCADE-Grande (2003/12-2011/10) SIBYLL2.3", "KASCADE-Grande_SIBYLL-2.3_allParticle_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "KASCADE-Grande", "KASCADE-Grande (2004/03-2012/05) QGSJet-II-2", "KASCADE-Grande_QGSJet-II-2_allParticle_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "KASCADE-Grande", "KASCADE-Grande (2004/03-2010/12) QGSJet-II-3", "KASCADE-Grande_QGSJet-II-3_allParticle_totalEnergy.txt")
+    dump_datafile("AllParticles", "ETOT", "KASCADE-Grande", "KASCADE-Grande (2003/01-2009/03) QGSJet-II-04", "KASCADE-Grande_QGSJet-II-04_allParticle_totalEnergy.txt")
+
+if __name__== "__main__":
+#    AMS02()
+#    ATIC()
+#    BESS()
+#    CALET()
+#    CREAM()
+#    DAMPE()
+#    FERMI()
+#    GAMMA()
+#    HAWC()
+#    HESS()
+#    ICETOP()
+#    KASCADE()
+    KASCADE_Grande()
+    # NUCLEON
+#    dump_datafile("H", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_H_totalEnergy.txt")
+#    dump_datafile("He", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_He_totalEnergy.txt")
+#    dump_datafile("C", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_C_totalEnergy.txt")
+#    dump_datafile("O", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_O_totalEnergy.txt")
+#    dump_datafile("Ne", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_Ne_totalEnergy.txt")
+#    dump_datafile("Mg", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_Mg_totalEnergy.txt")
+#    dump_datafile("Si", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_Si_totalEnergy.txt")
+#    dump_datafile("Fe", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_Fe_totalEnergy.txt")
+#    dump_datafile("AllParticles", "ETOT", "NUCLEON", "NUCLEON-KLEM (2015/07-2017/06)", "NUCLEON_allParticle_totalEnergy.txt")
+#    # PAMELA
+#    dump_datafile("e-", "EK", "PAMELA", "PAMELA (2006/07-2010/01)", "PAMELA_e-_kineticEnergy.txt")
+#    dump_datafile("e+", "EK", "PAMELA", "PAMELA (2006/07-2009/12)", "PAMELA_e+_kineticEnergy.txt")
+#    dump_datafile("e+/e-+e+", "EK", "PAMELA", "PAMELA (2006/07-2009/12)", "PAMELA_posfraction_kineticEnergy.txt")
+#    dump_datafile("1H-bar", "EKN", "PAMELA", "PAMELA (2006/07-2009/12)", "PAMELA_pbar_kineticEnergy.txt")
+#    dump_datafile("H", "R", "PAMELA", "PAMELA (2006/07-2008/12)", "PAMELA_H_rigidity.txt")
+#    dump_datafile("He", "R", "PAMELA", "PAMELA (2006/07-2008/12)", "PAMELA_He_rigidity.txt")
+#    dump_datafile("C", "R", "PAMELA", "PAMELA (2006/07-2008/03)", "PAMELA_C_rigidity.txt")
+#    dump_datafile("B/C", "EKN", "PAMELA", "PAMELA (2006/07-2008/03)", "PAMELA_B_C_kineticEnergyPerNucleon.txt")
+#    dump_datafile("B/C", "R", "PAMELA", "PAMELA (2006/07-2008/03)", "PAMELA_B_C_rigidity.txt")
+#    # TELESCOPE ARRAY
+#    dump_datafile("AllParticles", "ETOT", "Telescope", "Telescope Array Hybrid (2008/01-2015/05)", "TA_allParticle_totalEnergy.txt")
+#    # TIBET
+#    dump_datafile("H-He-group", "ETOT", "Tibet", "Tibet III (2000/11-2004/10) QGSJet01", "Tibet_light_totalEnergy.txt")
+#    # TRACER
+#    dump_datafile("B", "EKN", "TRACER", "TRACER (2006/07)", "TRACER_B_kineticEnergyPerNucleon.txt")
+#    dump_datafile("C", "EKN", "TRACER", "TRACER (2006/07)", "TRACER_C_kineticEnergyPerNucleon.txt")
+#    dump_datafile("O", "EKN", "TRACER", "TRACER (2006/07)", "TRACER_O_kineticEnergyPerNucleon.txt")
+#    dump_datafile("Ne", "EKN", "TRACER", "TRACER (2003/12)", "TRACER_Ne_kineticEnergyPerNucleon.txt")
+#    dump_datafile("Mg", "EKN", "TRACER", "TRACER (2003/12)", "TRACER_Mg_kineticEnergyPerNucleon.txt")
+#    dump_datafile("Si", "EKN", "TRACER", "TRACER (2003/12)", "TRACER_Si_kineticEnergyPerNucleon.txt")
+#    dump_datafile("Fe", "EKN", "TRACER", "TRACER (2006/07)", "TRACER_Fe_kineticEnergyPerNucleon.txt")
+#    dump_datafile("B/C", "EKN", "TRACER", "TRACER (2006/07)", "TRACER_B_C_kineticEnergyPerNucleon.txt")
+#    # TUNKA
+#    dump_datafile("H", "ETOT", "TUNKA", "TUNKA-133 Array (2009/10-2012/04) QGSJet01", "TUNKA-133_H_totalEnergy.txt", 1)
+#    dump_datafile("He", "ETOT", "TUNKA", "TUNKA-133 Array (2009/10-2012/04) QGSJet01", "TUNKA-133_He_totalEnergy.txt", 1)
+#    dump_datafile("AllParticles", "ETOT", "TUNKA", "TUNKA-133 Array (2009/10-2012/04) QGSJet01", "TUNKA-133_allParticle_totalEnergy.txt")
+
